f46746c721942c6c09711426f8cba367d4b0986c
Miguel Sánchez <122533153+miguel-dev01@users.noreply.github.com>
2024-09-09 16:39:54 +0200

    Add files via upload

---
+++ Shared/Movimiento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazor_test.Shared
+{
+    public class Movimiento
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public Double Cantidad { get; set; }
+
+        public String Tipo { get; set; } = String.Empty; // Ingreso o Egreso
+
+        public String Descripcion { get; set; } = String.Empty;
+
+        public Usuario? Usuario { get; set; }
+
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ Shared/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazor_test.Shared
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+    }
+}
